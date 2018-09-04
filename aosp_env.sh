1319bd9b909aa2001297210c8a7fdc7f063a19c5
perryes <perryes@hotmail.com>
2018-09-04 16:06:29 +0800

    dev

--- aosp_env.sh
+++ aosp_env.sh
@@ -1,9 +1,10 @@
 
-sudo apt-get install openjdk-8-jdk
+#初始化 AOSP 环境
 
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+sudo apt-get install openjdk-8-jdk -y
 
-git clone https://github.com/phhusson/treble_experimentations.git
+sudo apt-get install -y git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
 
 sudo apt-get install repo -y
 
+git clone https://github.com/phhusson/treble_experimentations.git
