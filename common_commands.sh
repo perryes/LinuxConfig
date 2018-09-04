ed7c42861279327efc236279a3cd2eea98e3a3b3
perryes <perryes@hotmail.com>
2018-09-04 15:58:12 +0800

    add common_commands.sh

---
+++ common_commands.sh
@@ -0,0 +1,30 @@
+# 
+
+
+1. du
+
+  descriptions: show the size of folders
+
+  options:
+
+	-h human readable
+	-d depth usually 1
+
+ example :
+
+ 	du -h -d 1 folderpath
+
+ 2. fdisk 
+
+ 	descriptions: manage disk on linux
+
+ 	options:
+
+ 		-l list all disk information
+
+ 	
+ 	command:
+ 		n create new partition
+ 		p primary partition
+
+ 3. 
