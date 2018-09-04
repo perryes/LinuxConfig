de0e0f56b3b50212cfdf2fba53e9cdcd211a5ebe
perryes <perryes@hotmail.com>
2018-09-04 17:21:50 +0800

    merge upstream

--- common_commands.sh
+++ common_commands.sh
@@ -27,4 +27,25 @@
  		n create new partition
  		p primary partition
 
- 3. 
+ 3.  add ssh key to github.com
+
+ 		1) copy rsa pub key to clipboard
+		2）paste to github settings
+		3) execute 'ssh -T git@github.com' to check result.
+		4) clone repository using .git url
+ 
+ 4. merge with forked upstream  
+
+ 	1) add remote repository
+		git remote -v
+		git remote add upstream 'remote url'
+		git remote -v
+
+	2) git fetch upstream master
+
+	3) checkout local master
+
+		git checkout master
+		
+	4) merge
+		git merge upstream/master
